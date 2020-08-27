94c0f444db8d47d70766848136ea73587ab558c9
TheBlueFireFox <adrian.hornung@yahoo.de>
2020-08-27 17:28:39 +0200

    partial adjestment to opcode trait implementation

--- chip8_lib/src/chip8.rs
+++ chip8_lib/src/chip8.rs
@@ -108,76 +108,18 @@ impl<T: DisplayCommands, U: KeybordCommands> ChipSet<T, U> {
     }
 
     /// will advance the program by a single step
-    pub fn step(&mut self) {
+    pub fn step(&mut self) -> Result<(), String> {
         // get next opcode
         self.set_opcode();
 
-        self.calc();
+        self.calc(self.opcode)
     }
 
-    /// will calculate the programs step by a single step
-    fn calc(&mut self) {
-        match self.opcode & OPCODE_MASK_F000 {
-            0x0000 => {
-                self.zero();
-            }
-            0x1000 => {
-                self.one();
-            }
-            0x2000 => {
-                self.two();
-            }
-            0x3000 => {
-                self.three();
-            }
-            0x4000 => {
-                self.four();
-            }
-            0x5000 => {
-                self.five();
-            }
-            0x6000 => {
-                self.six();
-            }
-            0x7000 => {
-                self.seven();
-            }
-            0x8000 => {
-                self.eight();
-            }
-            0x9000 => {
-                self.nine();
-            }
-            0xA000 => {
-                self.a();
-            }
-            0xB000 => {
-                self.b();
-            }
-            0xC000 => {
-                self.c();
-            }
-            0xD000 => {
-                self.d();
-            }
-            0xE000 => {
-                self.e();
-            }
-            0xF000 => {
-                self.f();
-            }
-            _ => {
-                panic!(format!(
-                    "An unsupported opcode was used {:#06X}",
-                    self.opcode
-                ));
-            }
-        }
-    }
 
     fn program_counter_step(&mut self, by: usize) {
         self.program_counter += by * PROGRAM_COUNTER_STEP;
     }
+   
     /// Will push the current pointer to the stack
     /// stack_counter is alwas one bigger then the
     /// entry it points to
@@ -221,18 +163,10 @@ impl<T: DisplayCommands, U: KeybordCommands> ChipSet<T, U> {
         self.opcode = opcode;
     }
 
-    #[cfg(test)]
-    /// special functions just for simpler testing used
-    /// as a wrapper function for calling the private
-    /// calc funcitonality
-    pub fn custom_calc(&mut self) {
-        self.calc();
-    }
 }
-
 impl<T: DisplayCommands, U: KeybordCommands> ChipOpcodes for ChipSet<T, U> {
-    fn zero(&mut self) -> Result<(), &'static str>{
-        match self.opcode {
+    fn zero(&mut self, opcode: Opcode) -> Result<(), String>{
+        match opcode {
             0x0E0 => {
                 // 00E0
                 // clear display
@@ -251,98 +185,103 @@ impl<T: DisplayCommands, U: KeybordCommands> ChipOpcodes for ChipSet<T, U> {
         Ok(())
     }
 
-    fn one(&mut self)  -> Result<(), &'static str>{
+    fn one(&mut self, opcode: Opcode)  -> Result<(), String>{
         // 1NNN
         // Jumps to address NNN.
-        self.program_counter = (self.opcode & OPCODE_MASK_0FFF) as usize;
+        self.program_counter = opcode.nnn();
+        Ok(())
     }
 
-    fn two(&mut self) -> Result<(), &'static str> {
+    fn two(&mut self, opcode: Opcode) -> Result<(), String> {
         // 2NNN
         // Calls subroutine at NNN
-
-        self.stack[self.stack_counter] = self.program_counter;
-        self.stack_counter += 1;
-        self.program_counter = (self.opcode & OPCODE_MASK_0FFF) as usize;
+        self.push_stack(self.program_counter)?;
+        self.program_counter = opcode.nnn();
+        Ok(())
     }
 
-    fn three(&mut self)  -> Result<(), &'static str>{
+    fn three(&mut self, opcode: Opcode)  -> Result<(), String>{
         // 3XNN
         // Skips the next instruction if VX equals NN. (Usually the next instruction is a jump to
         // skip a code block)
-        let (x, nn) = self.opcode.xnn();
+        let (x, nn) = opcode.xnn();
         if self.registers[x] == nn {
             self.program_counter_step(2);
         } else {
             self.program_counter_step(1);
         }
+        Ok(())
     }
 
-    fn four(&mut self)  -> Result<(), &'static str>{
+    fn four(&mut self, opcode: Opcode)  -> Result<(), String>{
         // 4XNN
         // Skips the next instruction if VX doesn't equal NN. (Usually the next instruction is a
         // jump to skip a code block)
-        let (x, nn) = self.opcode.xnn();
+        let (x, nn) = opcode.xnn();
         if self.registers[x] != nn {
             self.program_counter_step(2);
         } else {
             self.program_counter_step(1);
         }
+        Ok(())
     }
 
-    fn five(&mut self)  -> Result<(), &'static str>{
+    fn five(&mut self, opcode: Opcode)  -> Result<(), String>{
         // 5XY0
         // Skips the next instruction if VX equals VY. (Usually the next instruction is a jump to
         // skip a code block)
-        let (x, y) = self.opcode.xy();
+        let (x, y) = opcode.xy();
         if self.registers[x] == self.registers[y] {
             self.program_counter_step(2);
         } else {
             self.program_counter_step(1);
         }
+        Ok(())
     }
 
-    fn six(&mut self)  -> Result<(), &'static str>{
+    fn six(&mut self, opcode: Opcode)  -> Result<(), String>{
         // 6XNN
         // Sets VX to NN.
-        let (x, nn) = self.opcode.xnn();
+        let (x, nn) = opcode.xnn();
         self.registers[x] = nn;
         self.program_counter_step(1);
+        Ok(())
     }
 
-    fn seven(&mut self)  -> Result<(), &'static str>{
+    fn seven(&mut self, opcode: Opcode)  -> Result<(), String>{
         // 7XNN
         // Adds NN to VX. (Carry flag is not changed)
-        let (x, nn) = self.opcode.xnn();
+        let (x, nn) = opcode.xnn();
         self.registers[x] += nn;
         self.program_counter_step(1);
+        Ok(())
     }
 
-    fn eight(&mut self) -> Result<(), &'static str> {
+    fn eight(&mut self, opcode: Opcode) -> Result<(), String> {
         // remove the middle 8 bits for calculations
-        let (x, y) = self.opcode.xy();
-        match self.opcode & OPCODE_MASK_000F {
-            0x0000 => {
+        let (x, y,n ) = opcode.xyn();
+        match n {
+            0x0 => {
                 // 8XY0
                 // Sets VX to the value of VY.
                 self.registers[x] = self.registers[y];
             }
-            0x0001 => {
+            0x1 => {
                 // 8XY1
                 // Sets VX to VX or VY. (Bitwise OR operation)
                 self.registers[x] = self.registers[x] | self.registers[y];
             }
-            0x0002 => {
+            0x2 => {
                 // 8XY2
                 // Sets VX to VX and VY. (Bitwise AND operation)
                 self.registers[x] = self.registers[x] & self.registers[y];
             }
-            0x0003 => {
+            0x3 => {
                 // 8XY3
                 // Sets VX to VX xor VY.
                 self.registers[x] = self.registers[x] ^ self.registers[y];
             }
-            0x0004 => {
+            0x4 => {
                 // 8XY4
                 // Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there isn't.
                 let res = self.registers[x].checked_add(self.registers[y]);
@@ -361,7 +300,7 @@ impl<T: DisplayCommands, U: KeybordCommands> ChipOpcodes for ChipSet<T, U> {
                     }
                 };
             }
-            0x0005 => {
+            0x5 => {
                 // 8XY5
                 // VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there
                 // isn't.
@@ -381,14 +320,14 @@ impl<T: DisplayCommands, U: KeybordCommands> ChipOpcodes for ChipSet<T, U> {
                     }
                 };
             }
-            0x0006 => {
+            0x6 => {
                 // 8XY6
                 // Stores the least significant bit of VX in VF and then shifts VX to the right
                 // by 1.
                 self.registers[REGISTER_LAST] = self.registers[x] & 1;
                 self.registers[x] = self.registers[x] >> 1;
             }
-            0x0007 => {
+            0x7 => {
                 // 8XY7
                 // Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there
                 // isn't.
@@ -408,7 +347,7 @@ impl<T: DisplayCommands, U: KeybordCommands> ChipOpcodes for ChipSet<T, U> {
                     }
                 };
             }
-            0x000E => {
+            0xE => {
                 // 8XYE
                 // Stores the most significant bit of VX in VF and then shifts VX to the left by 1.
                 self.registers[REGISTER_LAST] = self.registers[x] & (1 << 7);
@@ -423,59 +362,67 @@ impl<T: DisplayCommands, U: KeybordCommands> ChipOpcodes for ChipSet<T, U> {
         }
         // increment the program counter by one
         self.program_counter_step(1);
+        Ok(())
     }
 
-    fn nine(&mut self)  -> Result<(), &'static str>{
+    fn nine(&mut self, opcode: Opcode)  -> Result<(), String>{
         // 9XY0
         // Skips the next instruction if VX doesn't equal VY. (Usually the next instruction is
         // a jump to skip a code block)
-        let (x, y) = self.opcode.xy();
+        let (x, y) = opcode.xy();
         if self.registers[x] != self.registers[y] {
             self.program_counter_step(2);
         } else {
             self.program_counter_step(1);
         }
+        Ok(())
     }
 
-    fn a(&mut self)  -> Result<(), &'static str>{
+    fn a(&mut self, opcode: Opcode)  -> Result<(), String>{
         // ANNN
         // Sets I to the address NNN.
-        self.index_register = self.opcode & OPCODE_MASK_0FFF;
+        self.index_register = opcode.nnn() as u16;
         self.program_counter_step(1);
+        Ok(())
     }
 
-    fn b(&mut self)  -> Result<(), &'static str>{
+    fn b(&mut self, opcode: Opcode)  -> Result<(), String> {
         // BNNN
         // Jumps to the address NNN plus V0.
+        let nnn = opcode.nnn();
         let v0 = self.registers[0] as usize;
-        self.program_counter = v0 + (self.opcode & OPCODE_MASK_0FFF) as usize;
+        self.program_counter = v0 + nnn;
         self.program_counter_step(1);
+        Ok(())
     }
 
-    fn c(&mut self)  -> Result<(), &'static str>{
+    fn c(&mut self, opcode: Opcode)  -> Result<(), String> {
         // CXNN
         // Sets VX to the result of a bitwise and operation on a random number (Typically: 0 to 255)
         // and NN.
-        let (x, nn) = self.opcode.xnn();
+        let (x, nn) = opcode.xnn();
         let rand = rand::random::<u8>();
         self.registers[x] = nn & rand;
         self.program_counter_step(1);
+        Ok(())
     }
 
-    fn d(&mut self)  -> Result<(), &'static str>{
+    fn d(&mut self, opcode: Opcode)  -> Result<(), String> {
         // DXYN
         // Draws a sprite at coordinate (VX, VY) that has a width of 8 pixels and a height of N
         // pixels. Each row of 8 pixels is read as bit-coded starting from memory location I; I
         // value doesn’t change after the execution of this instruction. As described above, VF is
         // set to 1 if any screen pixels are flipped from set to unset when the sprite is drawn, and
         // to 0 if that doesn’t happen
+        let (x,y,n) = opcode.xyn();
+        panic!("Not implemented!")
     }
 
-    fn e(&mut self)  -> Result<(), &'static str>{
-        let x = self.opcode.x();
+    fn e(&mut self, opcode: Opcode)  -> Result<(), String> {
+        let (x, nn) = opcode.xnn();
         let keyboard = self.keyboard.get_keybord();
-        let inc = match self.opcode & OPCODE_MASK_00FF {
-            0x009E => {
+        let inc = match nn {
+            0x9E => {
                 // EX9E
                 // Skips the next instruction if the key stored in VX is pressed. (Usually the next
                 // instruction is a jump to skip a code block)
@@ -485,7 +432,7 @@ impl<T: DisplayCommands, U: KeybordCommands> ChipOpcodes for ChipSet<T, U> {
                     1
                 }
             }
-            0x00A1 => {
+            0xA1 => {
                 // EXA1
                 // Skips the next instruction if the key stored in VX isn't pressed. (Usually the
                 // next instruction is a jump to skip a code block)
@@ -496,7 +443,7 @@ impl<T: DisplayCommands, U: KeybordCommands> ChipOpcodes for ChipSet<T, U> {
                 }
             }
             _ => {
-                panic!(format!(
+                return Err(format!(
                     "An unsupported opcode was used {:#06X?}",
                     self.opcode
                 ));
@@ -504,32 +451,33 @@ impl<T: DisplayCommands, U: KeybordCommands> ChipOpcodes for ChipSet<T, U> {
         };
 
         self.program_counter_step(inc);
+        Ok(())
     }
 
-    fn f(&mut self)  -> Result<(), &'static str>{
-        let x = self.opcode.x();
-        match self.opcode & OPCODE_MASK_FF00 {
-            0x007 => {
+    fn f(&mut self, opcode: Opcode)  -> Result<(), String>{
+        let (x, nn) = opcode.xnn();
+        match nn {
+            0x7 => {
                 // FX07
                 // Sets VX to the value of the delay timer.
                 self.registers[x] = self.delay_timer;
             }
-            0x00A => {
+            0xA => {
                 // FX0A
                 // A key press is awaited, and then stored in VX. (Blocking Operation. All
                 // instruction halted until next key event)
             }
-            0x0015 => {
+            0x15 => {
                 // FX15
                 // Sets the delay timer to VX.
                 self.delay_timer = self.registers[x];
             }
-            0x0018 => {
+            0x18 => {
                 // FX18
                 // Sets the sound timer to VX.
                 self.sound_timer = self.registers[x];
             }
-            0x001E => {
+            0x1E => {
                 // FX1E
                 // Adds VX to I. VF is set to 1 when there is a range overflow (I+VX>0xFFF), and to
                 // 0 when there isn't.
@@ -548,12 +496,12 @@ impl<T: DisplayCommands, U: KeybordCommands> ChipOpcodes for ChipSet<T, U> {
                     }
                 }
             }
-            0x0029 => {
+            0x29 => {
                 // FX29
                 // Sets I to the location of the sprite for the character in VX. Characters 0-F (in
                 // hexadecimal) are represented by a 4x5 font.
             }
-            0x0033 => {
+            0x33 => {
                 // FX33
                 // Stores the binary-coded decimal representation of VX, with the most significant
                 // of three digits at the address in I, the middle digit at I plus 1, and the least
@@ -567,7 +515,7 @@ impl<T: DisplayCommands, U: KeybordCommands> ChipOpcodes for ChipSet<T, U> {
                 self.memory[i + 1] = r / 10 % 10; // 246u8 / 10 => 24 % 10 => 4
                 self.memory[i + 2] = r % 10; // 246u8 % 10 => 6
             }
-            0x0055 => {
+            0x55 => {
                 // FX55
                 // Stores V0 to VX (including VX) in memory starting at address I. The offset from I
                 // is increased by 1 for each value written, but I itself is left unmodified.
@@ -576,7 +524,7 @@ impl<T: DisplayCommands, U: KeybordCommands> ChipOpcodes for ChipSet<T, U> {
                     self.memory[index + i] = self.registers[i];
                 }
             }
-            0x0065 => {
+            0x65 => {
                 // FX65
                 // Fills V0 to VX (including VX) with values from memory starting at address I. The
                 // offset from I is increased by 1 for each value written, but I itself is left
@@ -589,6 +537,7 @@ impl<T: DisplayCommands, U: KeybordCommands> ChipOpcodes for ChipSet<T, U> {
             _ => {}
         }
         self.program_counter_step(1);
+        Ok(())
     }
 }
 
@@ -690,11 +639,13 @@ mod tests {
         let key = devices::MockKeybordCommands::new();
 
         let mut chip = ChipSet::new(rom, dis, key);
+
         // set opcode
+        let opcode: Opcode = 0x00E0; 
         // setup chip state
-        chip.set_opcode_custom(0x0e0);
+        chip.set_opcode_custom(opcode);
         // run - if there was no panic it worked as intened
-        chip.custom_calc();
+        assert_eq!(chip.calc(opcode), Ok(()) );
     }
 
     #[test]
--- chip8_lib/src/opcode.rs
+++ chip8_lib/src/opcode.rs
@@ -14,32 +14,68 @@ pub const OPCODE_MASK_00FF: u16 = OPCODE_MASK_FFFF ^ OPCODE_MASK_FF00;
 /// the mask for the last four bytes
 pub const OPCODE_MASK_0FFF: u16 = OPCODE_MASK_FFFF ^ OPCODE_MASK_F000;
 
+/// a wrapper type for u16 to make it clear what is meant to be used
 pub type Opcode = u16;
 
+/// These are special traits used to filter out information
+/// from opcodes
 pub trait OpcodeTrait {
-    /// this is a opcode extractor for the opcode type `TXNN`
+
+    /// this is an opcode extractor that will return the
+    /// opcode number form any opcode
+    /// - `T` is the opcode type
+    fn t(&self) -> usize;
+
+    /// this is an opcode extractor for the opcode type `TNNN`
+    /// - `T` is the opcode type
+    /// - `NNN` is a register index
+    fn nnn(&self) -> usize;
+
+    /// this is an opcode extractor for the opcode type `TXNN`
     /// - `T` is the opcode type
     /// - `X` is a register index
     /// - `NN` is a constant
     fn xnn(&self) -> (usize, u8);
 
-    /// this is a opcode extractor for the opcode type `TXYT`
+    /// this is an opcode extractor for the opcode type `TXYN`
+    /// - `T` is the opcode type
+    /// - `X` is a register index
+    /// - `Y` is a constant
+    /// - `N` is a opcode subtype
+    fn xyn(&self) -> (usize, usize, usize);
+
+    /// this is an opcode extractor for the opcode type `TXYT`
     /// - `T` is the opcode type
     /// - `X` is a register index
     /// - `Y` is a constant
     fn xy(&self) -> (usize, usize);
 
-    /// this is a opcode extractor for the opcode type `TXTT`
+    /// this is an opcode extractor for the opcode type `TXTT`
     /// - `T` is the opcode type
     /// - `X` is a register index
     fn x(&self) -> usize;
 }
 
 impl OpcodeTrait for Opcode {
+
+    fn t(&self) -> usize {
+        (self & OPCODE_MASK_F000) as usize
+    }
+
+    fn nnn(&self) -> usize {
+        (self & OPCODE_MASK_0FFF) as usize
+    }
+
     fn xnn(&self) -> (usize, u8) {
         let x = self.x();
         let nn = (self & OPCODE_MASK_00FF) as u8;
         (x, nn)
+    }
+
+    fn xyn(&self) -> (usize, usize, usize) {
+        let (x,y) = self.xy();
+        let n = (self & OPCODE_MASK_000F) as usize;
+        (x,y,n)
     }
 
     fn xy(&self) -> (usize, usize) {
@@ -56,6 +92,67 @@ impl OpcodeTrait for Opcode {
 /// These are the traits that hava to be fullfilled for a working opcode
 /// table
 pub trait ChipOpcodes {
+
+    /// will calculate the programs step by a single step
+    fn calc(&mut self, opcode : Opcode) -> Result<(), String>{
+        match opcode.t() {
+            0x0000 => {
+                self.zero(opcode)
+            }
+            0x1000 => {
+                self.one(opcode)
+            }
+            0x2000 => {
+                self.two(opcode)
+            }
+            0x3000 => {
+                self.three(opcode)
+            }
+            0x4000 => {
+                self.four(opcode)
+            }
+            0x5000 => {
+                self.five(opcode)
+            }
+            0x6000 => {
+                self.six(opcode)
+            }
+            0x7000 => {
+                self.seven(opcode)
+            }
+            0x8000 => {
+                self.eight(opcode)
+            }
+            0x9000 => {
+                self.nine(opcode)
+            }
+            0xA000 => {
+                self.a(opcode)
+            }
+            0xB000 => {
+                self.b(opcode)
+            }
+            0xC000 => {
+                self.c(opcode)
+            }
+            0xD000 => {
+                self.d(opcode)
+            }
+            0xE000 => {
+                self.e(opcode)
+            }
+            0xF000 => {
+                self.f(opcode)
+            }
+            _ => {
+                panic!(format!(
+                    "An unsupported opcode was used {:#06X}",
+                    opcode
+                ));
+            }
+        }
+    }
+    
     /// A mutiuse opcode base for type `0NNN`
     ///
     /// - `0NNN` - Call     -                       - Calls machine code routine ([RCA 1802](https://en.wikipedia.org/wiki/RCA_1802) for COSMAC VIP) at address `NNN`. Not necessary for most ROMs.
@@ -63,35 +160,35 @@ pub trait ChipOpcodes {
     /// - `00EE` - Flow     - `return;`             - Returns from a subroutine.
     /// 
     /// Returns any possible error 
-    fn zero(&mut self) -> Result<(), &'static str>;
+    fn zero(&mut self, opcode : Opcode) -> Result<(), String>;
     /// - `1NNN` - Flow     - `goto NNN;`           - Jumps to address `NNN`.
     /// 
     /// Returns any possible error 
-    fn one(&mut self) -> Result<(), &'static str>;
+    fn one(&mut self, opcode : Opcode) -> Result<(), String>;
     /// - `2NNN` - Flow     - `*(0xNNN)()`          - Calls subroutine at `NNN`.
     /// 
     /// Returns any possible error 
-    fn two(&mut self) -> Result<(), &'static str>;
+    fn two(&mut self, opcode : Opcode) -> Result<(), String>;
     /// - `3XNN` - Cond 	- `if(Vx==NN)`          - Skips the next instruction if `VX` equals `NN`. (Usually the next instruction is a jump to skip a code block)
     /// 
     /// Returns any possible error 
-    fn three(&mut self) -> Result<(), &'static str>;
+    fn three(&mut self, opcode : Opcode) -> Result<(), String>;
     /// - `4XNN` - Cond     - `if(Vx!=NN)`          - Skips the next instruction if `VX` doesn' t equal `NN`. (Usually the next instruction is a jump to skip a code block)
     /// 
     /// Returns any possible error 
-    fn four(&mut self) -> Result<(), &'static str>;
+    fn four(&mut self, opcode : Opcode) -> Result<(), String>;
     /// - `5XY0` - Cond     - `if(Vx==Vy)`          - Skips the next instruction if `VX` equals `VY`. (Usually the next instruction is a jump to skip a code block)
     /// 
     /// Returns any possible error 
-    fn five(&mut self) -> Result<(), &'static str>;
+    fn five(&mut self, opcode : Opcode) -> Result<(), String>;
     /// - `6XNN` - Const    - `Vx = NN`             - Sets `VX` to `NN`.
     /// 
     /// Returns any possible error 
-    fn six(&mut self) -> Result<(), &'static str>;
+    fn six(&mut self, opcode : Opcode) -> Result<(), String>;
     /// - `7XNN` - Const    - `Vx += NN`            - Adds `NN` to `VX`. (Carry flag is not changed)
     /// 
     /// Returns any possible error 
-    fn seven(&mut self) -> Result<(), &'static str>;
+    fn seven(&mut self, opcode : Opcode) -> Result<(), String>;
     /// A mutiuse opcode base for type `8NNT` (T is a sub obcode)
     ///
     /// - `8XY0` - Assign   - `Vx=Vy`               - Sets `VX` to the value of `VY`.
@@ -105,34 +202,34 @@ pub trait ChipOpcodes {
     /// - `8XYE` - BitOp    - `Vx<<=1`              - Stores the most significant bit of `VX` in `VF` and then shifts `VX` to the left by `1`.
     /// 
     /// Returns any possible error 
-    fn eight(&mut self) -> Result<(), &'static str>;
+    fn eight(&mut self, opcode : Opcode) -> Result<(), String>;
     /// - `9XY0` - Cond     - `if(Vx!=Vy)`          - Skips the next instruction if `VX` doesn't equal `VY`. (Usually the next instruction is a jump to skip a code block)
     /// 
     /// Returns any possible error 
-    fn nine(&mut self) -> Result<(), &'static str>;
+    fn nine(&mut self, opcode : Opcode) -> Result<(), String>;
     /// - `ANNN` - MEM      - `I = NNN`             - Sets `I` to the address `NNN`.
     /// 
     /// Returns any possible error 
-    fn a(&mut self) -> Result<(), &'static str>;
+    fn a(&mut self, opcode : Opcode) -> Result<(), String>;
     /// - `BNNN` - Flow 	- `PC=V0+NNN`           - Jumps to the address `NNN` plus `V0`.
     /// 
     /// Returns any possible error 
-    fn b(&mut self) -> Result<(), &'static str>;
+    fn b(&mut self, opcode : Opcode) -> Result<(), String>;
     /// - `CXNN` - Rand     - `Vx=rand()&NN`        - Sets `VX` to the result of a bitwise and operation on a random number (Typically: `0 to 255`) and `NN`.
     /// 
     /// Returns any possible error 
-    fn c(&mut self) -> Result<(), &'static str>;
+    fn c(&mut self, opcode : Opcode) -> Result<(), String>;
     /// - `DXYN` - Disp     - `draw(Vx,Vy,N)`       - Draws a sprite at coordinate `(VX, VY)` that has a width of `8` pixels and a height of `N` pixels. Each row of `8` pixels is read as bit-coded starting from memory location `I`; `I` value doesn’t change after the execution of this instruction. As described above, `VF` is set to `1` if any screen pixels are flipped from set to unset when the sprite is drawn, and to `0` if that doesn’t happen
     /// 
     /// Returns any possible error 
-    fn d(&mut self) -> Result<(), &'static str>;
+    fn d(&mut self, opcode : Opcode) -> Result<(), String>;
     /// A mutiuse opcode base for type `EXTT` (T is a sub obcode)
     ///
     /// - `EX9E` - KeyOp    - `if(key()==Vx)`       - Skips the next instruction if the key stored in `VX` is pressed. (Usually the next instruction is a jump to skip a code block)
     /// - `EXA1` - KeyOp    - `if(key()!=Vx)`       - Skips the next instruction if the key stored in `VX` isn't pressed. (Usually the next instruction is a jump to skip a code block)
     /// 
     /// Returns any possible error 
-    fn e(&mut self) -> Result<(), &'static str>;
+    fn e(&mut self, opcode : Opcode) -> Result<(), String>;
     /// A mutiuse opcode base for type `FXTT` (T is a sub obcode)
     ///
     /// - `FX07` - Timer    - `Vx = get_delay()`    - Sets `VX` to the value of the delay timer.
@@ -146,5 +243,5 @@ pub trait ChipOpcodes {
     /// - `FX65` - MEM      - `reg_load(Vx,&I)`     - Fills `V0` to `VX` (including `VX`) with values from memory starting at address `I`. The offset from `I` is increased by `1` for each value written, but `I` itself is left unmodified.
     /// 
     /// Returns any possible error 
-    fn f(&mut self) -> Result<(), &'static str>;
+    fn f(&mut self, opcode : Opcode) -> Result<(), String>;
 }
