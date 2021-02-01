f9190a5960c4a348f24160b78e21ed30405494bd
TheBlueFireFox <adrian.hornung@yahoo.de>
2021-02-01 18:57:58 +0100

    change structure for WASM

---
+++ gui/src/lib.rs
@@ -0,0 +1,24 @@
+mod gui;
+
+// use chip::{chip8::ChipSet, resources::RomArchives};
+
+fn main() {
+    // let mut ra = RomArchives::new();
+    // let mut files = ra.file_names();
+    // files.sort();
+
+    // let chip = chip::ChipSet::new(); 
+
+    // let rom_name = &files[0].to_string();
+    // let rom = ra.get_file_data(rom_name).unwrap();
+
+    // let mut chip = ChipSet::new(rom);
+
+    // for i in 0..chip.get_keyboard().len() {
+    //     chip.set_key(i, i % 2 == 1);
+    // }
+
+    // println!("{}", chip);
+
+    
+}
