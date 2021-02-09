a877512b579b95a24f7727f28e3406f3dcc53ed7
Adrian Hornung <adrian.hornung@yahoo.de>
2021-02-09 13:26:08 +0100

    refactor to use web_sys and fix closure dereference

--- gui/src/timer.rs
+++ gui/src/timer.rs
@@ -1,10 +1,9 @@
 use chip::timer::TimedWorker;
-use wasm_bindgen::prelude::*;
+use wasm_bindgen::{prelude::*, JsCast};
+use web_sys::Window;
 
-#[wasm_bindgen]
-extern "C" {
-    fn setInterval(closure: &Closure<dyn FnMut()>, time: u32) -> i32;
-    fn clearInterval(id: i32);
+fn window() -> Window {
+    web_sys::window().expect("No `global` window found")
 }
 
 /// see here https://rustwasm.github.io/wasm-bindgen/api/wasm_bindgen/closure/struct.Closure.html#using-the-setinterval-api
@@ -27,14 +26,24 @@ impl TimedWorker for Worker {
         let function = Closure::wrap(Box::new(callback) as Box<dyn FnMut()>);
 
         // SAFETY: unwrap is safe here, as it is set a line above.
-        let interval_id = setInterval(&function, interval.as_millis() as u32);
 
+        let interval_id = window()
+            .set_interval_with_callback_and_timeout_and_arguments_0(
+                function.as_ref().unchecked_ref(),
+                interval.as_millis() as i32,
+            )
+            .expect("something went wrong");
         self.interval_id = Some(interval_id);
+        // SAFETY: Attention leaks memory, but as the system shall support both
+        // threaded and set intervall and a Closure is not Send no other option
+        // is available.
+        // Once WEAK_REF is supported this problem will be solved.
+        function.forget();
     }
 
     fn stop(&mut self) {
         if let Some(id) = self.interval_id.take() {
-            clearInterval(id);
+            window().clear_interval_with_handle(id);
         }
     }
 
