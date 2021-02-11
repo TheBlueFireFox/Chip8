1a7ad9f225e4a6811f7697e49a53d020edd1ecdd
Adrian Hornung <hornung.adrian@gmail.com>
2021-02-11 17:35:45 +0100

    refactor external wasm functions

--- gui/src/lib.rs
+++ gui/src/lib.rs
@@ -4,48 +4,7 @@ extern crate alloc;
 mod controller;
 mod timer;
 mod wrappers;
+pub mod exported;
 
-use alloc::string::String;
-use wasm_bindgen::prelude::*;
-
-use chip::{resources::RomArchives, devices::DisplayCommands};
 pub use wrappers::*;
-
-pub fn setup() -> Result<(), JsValue> {
-    let document = document(&window());
-    let body = body(&document);
-
-    // create elements
-    let val = document.create_element("p")?;
-    val.set_inner_html("Hello from Rust");
-    body.append_child(&val)?;
-
-    // get rom names
-    let ra = RomArchives::new();
-    let mut files = ra.file_names();
-    files.sort();
-
-    
-
-    Ok(())
-}
-
-#[wasm_bindgen]
-pub fn main(rom_name: String) -> Result<(), JsValue> {
-    let mut ra = RomArchives::new();
-
-    let rom = ra
-        .get_file_data(&rom_name)
-        .expect("Some unknown rom name was used to get the file data.");
-
-    let run_wrapper = RunWrapper::new(rom);
-    let chip = &mut *run_wrapper.chipset.borrow_mut();
-
-    run_wrapper.display.borrow().display(&chip.get_display());
-
-    // let val = document.create_element("pre")?;
-    // val.set_inner_html(&format!("{}", chip));
-    // body.append_child(&val)?;
-
-    Ok(())
-}
+pub use exported::*;
